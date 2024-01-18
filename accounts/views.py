1cc3cd443db0385eb2ec33944e8d27620668a413
Sanjit Sarkar <sanjit.est14@gmail.com>
2024-01-18 23:05:49 +0600

    updated

--- accounts/views.py
+++ accounts/views.py
@@ -35,7 +35,7 @@ class UserRegistrationView(FormView):
         print("token  ", token)
         uid = urlsafe_base64_encode(force_bytes(user.pk))
         print("uid  ", uid)
-        confirm_link = f"http://127.0.0.1:8000/books/active/{uid}/{token}"
+        confirm_link = f"https://national-library-umob.onrender.com/books/active/{uid}/{token}"
         email_subject = "Confirm Your Email"
         email_body = render_to_string(
             "accounts/confirm_email.html", {"confirm_link": confirm_link})
